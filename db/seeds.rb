79221c3b52da9d01f19d8b792fd1b43692d87641
Tomas Madariaga <critimass@gmail.com>
2016-01-16 19:06:30 +0100

    seeds.rb filled with datasets loading

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'nokogiri'
+require 'uri'
+require 'net/http'
+
+#Load Canada British Columbia datasets related with "water" word
+
+datasets_url = "http://apis.gov.bc.ca/bcdc/3/action/package_search?q=%22water%22"
+resp = Net::HTTP.get_response URI.parse(datasets_url)
+result = JSON.parse resp.body
+
+datasets = result["result"]["results"]
+
+datasets.each do |d|
+	dataset = Dataset.new
+	dataset.object_name = d["object_name"]
+	dataset.name = d["title"]
+	dataset.license_title = d["license_title"]
+	dataset.license_url = d["license_url"]
+	dataset.save
+	puts "Created Dataset #{dataset.object_name}"
+end
+
+puts "Datasets created"
